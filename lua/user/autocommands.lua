5aaaef9f0cefac2a1e1af8aa01c206fd4f9da94a
Marvin Velásquez <immavlgt@gmail.com>
2022-01-17 14:42:08 -0800

    Update autocommands.lua

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,3 +1,24 @@
+local au = require('au')
+
+    -- disable numbers for terminal
+    au({ 'TermOpen', 'TermEnter' }, {
+        '*',
+        function()
+          vim.wo.relativenumber = false
+          vim.wo.number = false
+        end,
+    })
+    -- enable numbers on active Tabs/Windows
+    au({ 'TabEnter', 'WinEnter' }, {
+        '*',
+        function()
+          vim.wo.relativenumber = true
+          vim.wo.number = true
+        end,
+    })
+  -- }
+
+
 vim.cmd [[
   augroup _general_settings
     autocmd!
