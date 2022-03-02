1a8a9b42ff7141620fa4263b7e1a24698e9cf3f1
Marvin Velasquez <immavlgt@gmail.com>
2022-03-02 15:50:31 -0600

    feat(orgmode): configuring org mode

--- lua/user/org.lua
+++ lua/user/org.lua
@@ -1,31 +1,21 @@
-local pc = require 'nvim-treesitter.parsers'.get_parser_configs()
-pc.org = {
-  install_info = {
-
-    url = 'https://github.com/milisims/tree-sitter-org',
-    revision = 'f110024d539e676f25b72b7c80b0fd43c34264ef',
-    files = {'src/parser.c', 'src/scanner.cc'},
-  },
-  filetype = 'org'
-}
+require('orgmode').setup_ts_grammar()
 
 require'nvim-treesitter.configs'.setup {
-  -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
   highlight = {
     enable = true,
-    disable = {'org'}, -- Remove this to use TS highlighter for some of the highlights (Experimental)
-    additional_vim_regex_highlighting = {'org'}, -- Required since TS highlighter doesn't support all syntax features (conceal)
+    disable = {'org'},
+    additional_vim_regex_highlighting = {'org'},
   },
-  ensure_installed = {'org'}, -- Or run :TSUpdate org
+  ensure_installed = {'org'},
 }
 
 require('orgmode').setup({
-  org_agenda_files = {'~/Documents/org/*'},
-  org_default_notes_file = '~/Documents/refile.org',
+  org_agenda_files = {'~/Documents/agenda'},
+  org_default_notes_file = '~/Documents/agenda/refile.org',
   mappings = {
     global = {
       org_agenda = 'gA',
-      org_capture = 'gC'
+      org_capture = 'gC',
     }
   }
 })
