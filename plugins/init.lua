02652743f496c48946f89b4cf90baf54a764f496
Marvin Velasquez <immavlgt@gmail.com>
2022-08-04 09:38:41 -0600

    feat(lazygit): can be enabled in all contexts

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,44 +1,43 @@
 return {
   ["kdheepak/lazygit.nvim"] = {
     after = "telescope.nvim",
-    setup = function ()
-      require("core.lazy_load").on_file_open("lazygit.nvim")
+    setup = function()
+      require("core.lazy_load").on_file_open "lazygit.nvim"
     end,
-    config = function ()
+    config = function()
       require "custom.plugins.lazygit"
     end,
   },
   ["jose-elias-alvarez/null-ls.nvim"] = {
     after = "nvim-lspconfig",
-    config = function ()
+    config = function()
       require "custom.plugins.null-ls"
-    end
+    end,
   },
   ["neovim/nvim-lspconfig"] = {
-    setup = function ()
+    setup = function()
       require("core.lazy_load").on_file_open "nvim-lspconfig"
     end,
-    config = function ()
-      require("plugins.configs.lspconfig")
-      require("custom.plugins.lspconfig")
-    end
-  },
-  ['rodjek/vim-puppet'] = {
-    opt = true,
-    ft = "puppet"
-  },
-  ['goolord/alpha-nvim'] = {
-    disable = false,
-    command = "Alpha"
-  },
-  ['nvim-neorg/neorg'] = {
-    after = "nvim-treesitter",
-    setup = function ()
-      require("core.lazy_load").on_file_open("neorg")
-      require("core.lazy_load").on_file_open("plenary.nvim")
-      require("core.lazy_load").on_file_open("nvim-treesitter")
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.plugins.lspconfig"
     end,
-    ft = "norg",
+  },
+  ["rodjek/vim-puppet"] = {
+    opt = true,
+    ft = "puppet",
+  },
+  ["goolord/alpha-nvim"] = {
+    disable = false,
+    command = "Alpha",
+  },
+  ["nvim-neorg/neorg"] = {
+    after = "nvim-treesitter",
+    setup = function()
+      require("core.lazy_load").on_file_open "neorg"
+      require("core.lazy_load").on_file_open "plenary.nvim"
+      require("core.lazy_load").on_file_open "nvim-treesitter"
+    end,
     config = function()
       require "custom.plugins.neorgconfig"
     end,
