0190f4c1e12fa38aabcc6e702851e63df9cae538
Marvin Velasquez <immavlgt@gmail.com>
2022-08-11 11:12:09 -0600

    feat(highlights): add Norg highlights

--- chadrc.lua
+++ chadrc.lua
@@ -1,17 +1,19 @@
 local M = {}
-local overrides = require('custom.plugins.overrides')
+local overrides = require "custom.plugins.overrides"
 
 M.ui = {
-   theme = "catppuccin",
-   theme_toggle = { "catppuccin", "ayu-dark"},
+  theme = "catppuccin",
+  hl_add = require("custom.highlights").new_hlgroups,
+  hl_override = require("custom.highlights").new_overriden_hlgroups,
+  theme_toggle = { "catppuccin", "ayu-dark" },
 }
 
-M.mappings = require 'custom.plugins.keymaps'
+M.mappings = require "custom.plugins.keymaps"
 
 M.plugins = {
-  user = require 'custom.plugins',
+  user = require "custom.plugins",
   override = {
-    ['kyazdani42/nvim-tree.lua'] = overrides.nvimtree,
+    ["kyazdani42/nvim-tree.lua"] = overrides.nvimtree,
     ["nvim-treesitter/nvim-treesitter"] = overrides.treesitter,
     ["williamboman/mason.nvim"] = overrides.mason,
   },
