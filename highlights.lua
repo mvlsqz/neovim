83d1db13acf4030e35decfdd247e6fe460c1aa00
Marvin Velàsquez <mvelasquez@paypal.com>
2022-12-05 17:49:43 -0600

    fix(schema): neorg color schema

--- highlights.lua
+++ highlights.lua
@@ -9,6 +9,12 @@ local color = {
   magenta = "#d2a6ff",
   cyan = "#95e6cb",
   white = "#e6e1cf",
+  first = "#ffb454",
+  second = "#59c2ff",
+  third = "#d2a6ff",
+  forth = "#b8cc52",
+  fifth = "#95e6cb",
+  sixth = "#e6e1cf",
   bright_black = "#272d38",
   bright_red = "#f07178",
   bright_green = "#b8cc52",
@@ -16,7 +22,7 @@ local color = {
   bright_blue = "#95e6cb",
   bright_magenta = "#d2a6ff",
   bright_cyan = "#e6b673",
-  bright_withe = "#e6e1cf",
+  bright_white = "#e6e1cf",
 }
 
 local hi = {}
@@ -35,60 +41,60 @@ hi["@neorg.markup.variable"] = { fg = color.yellow, style = "italic" }
 hi["@neorg.markup.spoiler"] = { fg = color.blue }
 hi["@neorg.markup.inline_comment"] = { fg = color.bright_magenta }
 hi["@neorg.markup.inline_math"] = { fg = color.blue }
-hi["@neorg.headings.1.prefix"] = { fg = color.white, style = "bold" }
-hi["@neorg.headings.2.prefix"] = { fg = color.red, style = "bold" }
-hi["@neorg.headings.3.prefix"] = { fg = color.green, style = "bold" }
-hi["@neorg.headings.4.prefix"] = { fg = color.blue, style = "bold" }
-hi["@neorg.headings.5.prefix"] = { fg = color.magenta, style = "bold" }
-hi["@neorg.headings.6.prefix"] = { fg = color.yellow, style = "bold" }
-hi["@neorg.headings.1.title"] = { fg = color.white, style = "bold" }
-hi["@neorg.headings.2.title"] = { fg = color.red, style = "bold" }
-hi["@neorg.headings.3.title"] = { fg = color.green, style = "bold" }
-hi["@neorg.headings.4.title"] = { fg = color.blue, style = "bold" }
-hi["@neorg.headings.5.title"] = { fg = color.magenta, style = "bold" }
-hi["@neorg.headings.6.title"] = { fg = color.yellow, style = "bold" }
-hi["@neorg.lists.unordered.1.prefix"] = { fg = color.white }
-hi["@neorg.lists.unordered.2.prefix"] = { fg = color.red }
-hi["@neorg.lists.unordered.3.prefix"] = { fg = color.green }
-hi["@neorg.lists.unordered.4.prefix"] = { fg = color.blue }
-hi["@neorg.lists.unordered.5.prefix"] = { fg = color.magenta }
-hi["@neorg.lists.unordered.6.prefix"] = { fg = color.yellow }
-hi["@neorg.lists.unordered.1.content"] = { fg = color.white }
-hi["@neorg.lists.unordered.2.content"] = { fg = color.red }
-hi["@neorg.lists.unordered.3.content"] = { fg = color.green }
-hi["@neorg.lists.unordered.4.content"] = { fg = color.blue }
-hi["@neorg.lists.unordered.5.content"] = { fg = color.magenta }
-hi["@neorg.lists.unordered.6.content"] = { fg = color.yellow }
-hi["@neorg.lists.ordered.1.prefix"] = { fg = color.white }
-hi["@neorg.lists.ordered.2.prefix"] = { fg = color.red }
-hi["@neorg.lists.ordered.3.prefix"] = { fg = color.green }
-hi["@neorg.lists.ordered.4.prefix"] = { fg = color.blue }
-hi["@neorg.lists.ordered.5.prefix"] = { fg = color.magenta }
-hi["@neorg.lists.ordered.6.prefix"] = { fg = color.yellow }
-hi["@neorg.lists.ordered.1.content"] = { fg = color.white }
-hi["@neorg.lists.ordered.2.content"] = { fg = color.red }
-hi["@neorg.lists.ordered.3.content"] = { fg = color.green }
-hi["@neorg.lists.ordered.4.content"] = { fg = color.blue }
-hi["@neorg.lists.ordered.5.content"] = { fg = color.magenta }
-hi["@neorg.lists.ordered.6.content"] = { fg = color.yellow }
-hi["@neorg.links.unordered.1.prefix"] = { fg = color.white }
-hi["@neorg.links.unordered.2.prefix"] = { fg = color.red }
-hi["@neorg.links.unordered.3.prefix"] = { fg = color.green }
-hi["@neorg.links.unordered.4.prefix"] = { fg = color.blue }
-hi["@neorg.links.unordered.5.prefix"] = { fg = color.magenta }
-hi["@neorg.links.unordered.6.prefix"] = { fg = color.yellow }
-hi["@neorg.links.ordered.1.prefix"] = { fg = color.white }
-hi["@neorg.links.ordered.2.prefix"] = { fg = color.red }
-hi["@neorg.links.ordered.3.prefix"] = { fg = color.green }
-hi["@neorg.links.ordered.4.prefix"] = { fg = color.blue }
-hi["@neorg.links.ordered.5.prefix"] = { fg = color.magenta }
-hi["@neorg.links.ordered.6.prefix"] = { fg = color.yellow }
-hi["@neorg.quotes.1.prefix"] = { fg = color.white }
-hi["@neorg.quotes.2.prefix"] = { fg = color.red }
-hi["@neorg.quotes.3.prefix"] = { fg = color.green }
-hi["@neorg.quotes.4.prefix"] = { fg = color.blue }
-hi["@neorg.quotes.5.prefix"] = { fg = color.magenta }
-hi["@neorg.quotes.6.prefix"] = { fg = color.yellow }
+hi["@neorg.headings.1.prefix"] = { fg = color.first, style = "bold" }
+hi["@neorg.headings.2.prefix"] = { fg = color.second, style = "bold" }
+hi["@neorg.headings.3.prefix"] = { fg = color.third, style = "bold" }
+hi["@neorg.headings.4.prefix"] = { fg = color.forth, style = "bold" }
+hi["@neorg.headings.5.prefix"] = { fg = color.fifth, style = "bold" }
+hi["@neorg.headings.6.prefix"] = { fg = color.sixth, style = "bold" }
+hi["@neorg.headings.1.title"] = { fg = color.first, style = "bold" }
+hi["@neorg.headings.2.title"] = { fg = color.second, style = "bold" }
+hi["@neorg.headings.3.title"] = { fg = color.third, style = "bold" }
+hi["@neorg.headings.4.title"] = { fg = color.forth, style = "bold" }
+hi["@neorg.headings.5.title"] = { fg = color.fifth, style = "bold" }
+hi["@neorg.headings.6.title"] = { fg = color.sixth, style = "bold" }
+hi["@neorg.lists.unordered.1.prefix"] = { fg = color.first }
+hi["@neorg.lists.unordered.2.prefix"] = { fg = color.second }
+hi["@neorg.lists.unordered.3.prefix"] = { fg = color.third }
+hi["@neorg.lists.unordered.4.prefix"] = { fg = color.forth }
+hi["@neorg.lists.unordered.5.prefix"] = { fg = color.fifth }
+hi["@neorg.lists.unordered.6.prefix"] = { fg = color.sixth }
+hi["@neorg.lists.unordered.1.content"] = { fg = color.first }
+hi["@neorg.lists.unordered.2.content"] = { fg = color.second }
+hi["@neorg.lists.unordered.3.content"] = { fg = color.third }
+hi["@neorg.lists.unordered.4.content"] = { fg = color.forth }
+hi["@neorg.lists.unordered.5.content"] = { fg = color.fifth }
+hi["@neorg.lists.unordered.6.content"] = { fg = color.sixth }
+hi["@neorg.lists.ordered.1.prefix"] = { fg = color.first }
+hi["@neorg.lists.ordered.2.prefix"] = { fg = color.second }
+hi["@neorg.lists.ordered.3.prefix"] = { fg = color.third }
+hi["@neorg.lists.ordered.4.prefix"] = { fg = color.forth }
+hi["@neorg.lists.ordered.5.prefix"] = { fg = color.fifth }
+hi["@neorg.lists.ordered.6.prefix"] = { fg = color.sixth }
+hi["@neorg.lists.ordered.1.content"] = { fg = color.first }
+hi["@neorg.lists.ordered.2.content"] = { fg = color.second }
+hi["@neorg.lists.ordered.3.content"] = { fg = color.third }
+hi["@neorg.lists.ordered.4.content"] = { fg = color.forth }
+hi["@neorg.lists.ordered.5.content"] = { fg = color.fifth }
+hi["@neorg.lists.ordered.6.content"] = { fg = color.sixth }
+hi["@neorg.links.unordered.1.prefix"] = { fg = color.first }
+hi["@neorg.links.unordered.2.prefix"] = { fg = color.second }
+hi["@neorg.links.unordered.3.prefix"] = { fg = color.third }
+hi["@neorg.links.unordered.4.prefix"] = { fg = color.forth }
+hi["@neorg.links.unordered.5.prefix"] = { fg = color.fifth }
+hi["@neorg.links.unordered.6.prefix"] = { fg = color.sixth }
+hi["@neorg.links.ordered.1.prefix"] = { fg = color.first }
+hi["@neorg.links.ordered.2.prefix"] = { fg = color.second }
+hi["@neorg.links.ordered.3.prefix"] = { fg = color.third }
+hi["@neorg.links.ordered.4.prefix"] = { fg = color.forth }
+hi["@neorg.links.ordered.5.prefix"] = { fg = color.fifth }
+hi["@neorg.links.ordered.6.prefix"] = { fg = color.sixth }
+hi["@neorg.quotes.1.prefix"] = { fg = color.first }
+hi["@neorg.quotes.2.prefix"] = { fg = color.second }
+hi["@neorg.quotes.3.prefix"] = { fg = color.third }
+hi["@neorg.quotes.4.prefix"] = { fg = color.forth }
+hi["@neorg.quotes.5.prefix"] = { fg = color.fifth }
+hi["@neorg.quotes.6.prefix"] = { fg = color.sixth }
 
 for group, colors in pairs(hi) do
   vim.cmd(
