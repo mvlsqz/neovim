0005c3b4cfa8dee25296ee54472f5105a9959931
Marvin Velasquez <immavlgt@gmail.com>
2023-02-19 11:04:29 -0600

    feat(notes): add neorg for note taking

--- lua/plugins/mind.lua
+++ lua/plugins/mind.lua
@@ -3,6 +3,28 @@ return {
   branch = "v2.2",
   requires = { "nvim-lua/plenary.nvim" },
   config = function()
-    require("mind").setup()
+    require("mind").setup({
+      edit = {
+        data_extension = ".norg",
+        data_header = "* %s",
+      },
+    })
   end,
+  dependencies = {
+    "nvim-neorg/neorg",
+    run = ":Neorg sync-parsers",
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {},
+          ["core.norg.concealer"] = {},
+          ["core.norg.completion"] = {
+            config = {
+              engine = "nvim-cmp",
+            },
+          },
+        },
+      })
+    end,
+  },
 }
