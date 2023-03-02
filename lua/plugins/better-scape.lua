4c3b252f25b600737aeb5bdd6b0ee8903e18edcd
Marvin Velàsquez <mvelasquez@paypal.com>
2023-03-02 15:09:37 -0600

    fix(Scape): add keybind

--- lua/plugins/better-scape.lua
+++ lua/plugins/better-scape.lua
@@ -2,7 +2,7 @@ return {
   "max397574/better-escape.nvim",
   config = function()
     require("better_escape").setup({
-      mapping = { "jk", "jj" },
+      mapping = { "jk", "jj", "JJ" },
       timeout = vim.o.timeoutlen,
       clear_empty_lines = true,
       keys = "<Esc>",
