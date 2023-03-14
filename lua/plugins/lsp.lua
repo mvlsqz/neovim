29cb149a4e186450c6f50fa76957bf3008a3ed43
Marvin Velàsquez <mvelasquez@paypal.com>
2023-03-14 13:45:14 -0700

    feat(improvements): multiple improvements

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,63 +1,81 @@
 return {
   {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = { "mason.nvim" },
+    opts = function()
+      local nls = require("null-ls")
+      return {
+        sources = {
+          sources = {
+            nls.builtins.diagnostics.puppet_lint,
+            nls.builtins.formatting.puppet_lint,
+            nls.builtins.diagnostics.rubocop,
+            nls.builtins.completion.spell,
+          },
+        },
+      }
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
     opts = {
-      ---@type lspconfig.options
-      ensure_installed = {
+      servers = {
+        ansiblels = {},
+        bashls = {},
         pyright = {},
         terraformls = {},
-        solargraph = {},
-      },
-      automatic_installation = {
-        exclude = { "solargraph" },
+        yamlls = {},
+        solargraph = {
+          mason = false,
+        },
       },
     },
-    {
-      "hrsh7th/nvim-cmp",
-      dependencies = {
-        "hrsh7th/cmp-emoji",
-      },
-      opts = function(_, opts)
-        local has_words_before = function()
-          unpack = unpack or table.unpack
-          local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-          return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-        end
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+    },
+    opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
 
-        local luasnip = require("luasnip")
-        local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      local cmp = require("cmp")
 
-        opts.window = {
-          completion = {},
-          documentation = {},
-        }
+      opts.window = {
+        completion = {},
+        documentation = {},
+      }
 
-        opts.mapping = vim.tbl_extend("force", opts.mapping, {
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+            ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
             -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
             -- they way you will only jump inside the snippet region
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-        })
-      end,
-    },
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+            ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+    end,
   },
 }
