3eb5fbbab46ca3b4ee82b9e8e8d457d3d9c4665d
Marvin Velàsquez <mvelasquez@paypal.com>
2023-10-31 14:08:59 -0600

    fix(navic): update events

--- lua/plugins/extra.lua
+++ lua/plugins/extra.lua
@@ -2,10 +2,31 @@ return {
   {
     "utilyre/barbecue.nvim",
     name = "barbecue",
+    event = { "LazyFile", "VeryLazy" },
     version = "*",
     keys = require("configs.keys").barbecue,
     dependencies = {
-      "SmiteshP/nvim-navic",
+      {
+        "SmiteshP/nvim-navic",
+        lazy = true,
+        init = function()
+          vim.g.navic_silence = true
+          require("lazyvim.util").lsp.on_attach(function(client, buffer)
+            if client.supports_method("textDocument/documentSymbol") then
+              require("nvim-navic").attach(client, buffer)
+            end
+          end)
+        end,
+        opts = function()
+          return {
+            separator = " ",
+            highlight = true,
+            depth_limit = 5,
+            icons = require("lazyvim.config").icons.kinds,
+            lazy_update_context = true,
+          }
+        end,
+      },
       "nvim-tree/nvim-web-devicons", -- optional dependency
     },
     config = function()
