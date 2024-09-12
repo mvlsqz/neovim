bff100675c7d6a5c01054d62fc7d34c7c55ff3e1
mvlsqz <mavelasquezl85@gmail.com>
2024-09-12 09:10:52 -0600

    feat(wezterm): add wezterm config

---
+++ wezterm/user_configs.lua
@@ -0,0 +1,183 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+M = {}
+M.font_size = 22
+M.keys = {
+	{
+		key = "LeftArrow",
+		mods = "OPT",
+		action = act({
+			SendKey = {
+				key = "b",
+				mods = "ALT",
+			},
+		}),
+	},
+	{
+		key = "RightArrow",
+		mods = "OPT",
+		action = act({
+			SendKey = {
+				key = "f",
+				mods = "ALT",
+			},
+		}),
+	},
+	{
+		key = [[\]],
+		mods = "ALT",
+		action = act({
+			SplitHorizontal = { domain = "CurrentPaneDomain" },
+		}),
+	},
+	{
+		key = [[-]],
+		mods = "ALT",
+		action = act({
+			SplitVertical = { domain = "CurrentPaneDomain" },
+		}),
+	},
+	{
+		key = "q",
+		mods = "SUPER",
+		action = act({ CloseCurrentPane = { confirm = true } }),
+	},
+	{
+		key = "h",
+		mods = "ALT",
+		action = act({ ActivatePaneDirection = "Left" }),
+	},
+	{
+		key = "l",
+		mods = "ALT",
+		action = act({ ActivatePaneDirection = "Right" }),
+	},
+	{
+		key = "k",
+		mods = "ALT",
+		action = act({ ActivatePaneDirection = "Up" }),
+	},
+	{
+		key = "j",
+		mods = "ALT",
+		action = act({ ActivatePaneDirection = "Down" }),
+	},
+	{
+		key = "h",
+		mods = "SUPER|ALT",
+		action = act({ AdjustPaneSize = { "Left", 2 } }),
+	},
+	{
+		key = "l",
+		mods = "SUPER|ALT",
+		action = act({ AdjustPaneSize = { "Right", 2 } }),
+	},
+	{
+		key = "k",
+		mods = "SUPER|ALT",
+		action = act({ AdjustPaneSize = { "Up", 2 } }),
+	},
+	{
+		key = "j",
+		mods = "SUPER|ALT",
+		action = act({ AdjustPaneSize = { "Down", 2 } }),
+	},
+	{
+		key = "t",
+		mods = "ALT",
+		action = act.SpawnTab("DefaultDomain"),
+	},
+	{
+		key = "g",
+		mods = "ALT",
+		action = act.SplitHorizontal({
+			domain = "CurrentPaneDomain",
+			args = { "lazygit" },
+			set_environment_variables = {
+				EDITOR = "nvim",
+				VISUAL = "nvim",
+			},
+		}),
+	},
+	{
+		key = "g",
+		mods = "ALT|SHIFT",
+		action = act.SplitVertical({
+			domain = "CurrentPaneDomain",
+			args = { "lazygit" },
+			set_environment_variables = {
+				EDITOR = "nvim",
+				VISUAL = "nvim",
+			},
+		}),
+	},
+	{
+		key = "n",
+		mods = "ALT",
+		action = act.SplitHorizontal({
+			domain = "CurrentPaneDomain",
+			args = { "nvim" },
+			set_environment_variables = {
+				EDITOR = "nvim",
+				VISUAL = "nvim",
+			},
+		}),
+	},
+	{
+		key = "r",
+		mods = "ALT",
+		action = act.PromptInputLine({
+			description = "Enter name for tab",
+			action = wezterm.action_callback(function(window, _, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+	{
+		key = "w",
+		mods = "ALT",
+		action = act({ CloseCurrentTab = { confirm = true } }),
+	},
+	{
+		key = "Tab",
+		mods = "ALT",
+		action = act({ ActivateTabRelative = 1 }),
+	},
+	{
+		key = "Tab",
+		mods = "ALT|SHIFT",
+		action = act({ ActivateTabRelative = -1 }),
+	}, -- standard copy/paste bindings
+	{
+		key = "m",
+		mods = "ALT",
+		action = act.ShowLauncherArgs({ flags = "FUZZY|LAUNCH_MENU_ITEMS|COMMANDS" }),
+	},
+	{
+		key = "x",
+		mods = "ALT",
+		action = "ActivateCopyMode",
+	},
+	{
+		key = "v",
+		mods = "SUPER",
+		action = act({ PasteFrom = "Clipboard" }),
+	},
+	{
+		key = "c",
+		mods = "SUPER",
+		action = act({ CopyTo = "ClipboardAndPrimarySelection" }),
+	},
+}
+
+for i = 1, 9 do
+	table.insert(M.keys, {
+		key = tostring(i),
+		mods = "CTRL|ALT",
+		action = act.MoveTab(i - 1),
+	})
+end
+
+return M
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,156 @@
+local wezterm = require("wezterm")
+local colors = require("custom_colors").colors
+local user_configs = require("user_configs")
+local my_colorscheme = colors.mocha
+
+local function font_with_fallback(name, params)
+	local names = {
+		{
+			family = name,
+			harfbuzz_features = {
+				"calt",
+				"liga",
+				"dlig",
+				"ss01",
+				"ss02",
+				"ss03",
+				"ss04",
+				"ss05",
+				"ss06",
+				"ss07",
+				"ss08",
+			},
+		},
+		"JetBrainsMono Nerd Font",
+		"Noto Emoji",
+		"Apple Color Emoji",
+	}
+	return wezterm.font_with_fallback(names, params)
+end
+
+local font_name = "Monaspace Argon"
+
+return {
+	unix_domains = {
+		{
+			name = "unix",
+		},
+	},
+	-- OpenGL for GPU acceleration, Software for CPU
+	front_end = "OpenGL",
+	window_decorations = "RESIZE",
+	font = font_with_fallback(font_name),
+	-- NOTE: -> Font Rules Mix and Match ->
+	font_rules = {
+		{
+			italic = false,
+			intensity = "Normal",
+			font = font_with_fallback("Monaspace Neon Var", { weight = "Regular" }),
+		},
+		{
+			intensity = "Bold",
+			font = font_with_fallback("Monaspace Krypton Var", { weight = "ExtraBold" }),
+		},
+		{
+			italic = true,
+			font = font_with_fallback("Monaspace Radon", { weight = "ExtraLight", italic = true }),
+		},
+	},
+	warn_about_missing_glyphs = false,
+	font_size = user_configs.font_size,
+	line_height = 1.0,
+
+	-- Cursor style
+	default_cursor_style = "BlinkingUnderline",
+
+	-- X11
+	enable_wayland = false,
+
+	-- Keybinds
+	disable_default_key_bindings = true,
+	keys = user_configs.keys,
+
+	-- Tokyo Night Colorscheme
+	bold_brightens_ansi_colors = true,
+	-- color_scheme = "Catppuccin Latte",
+	colors = {
+		foreground = my_colorscheme.text,
+		background = my_colorscheme.base,
+		cursor_bg = my_colorscheme.rosewater,
+		cursor_border = my_colorscheme.rosewater,
+		cursor_fg = my_colorscheme.crust,
+		selection_bg = my_colorscheme.surface2,
+		selection_fg = my_colorscheme.text,
+		scrollbar_thumb = my_colorscheme.surface2,
+		split = my_colorscheme.overlay0,
+		ansi = {
+			my_colorscheme.subtext1,
+			my_colorscheme.red,
+			my_colorscheme.green,
+			my_colorscheme.yellow,
+			my_colorscheme.blue,
+			my_colorscheme.pink,
+			my_colorscheme.teal,
+			my_colorscheme.surface2,
+		},
+		brights = {
+			my_colorscheme.subtext0,
+			my_colorscheme.red,
+			my_colorscheme.green,
+			my_colorscheme.yellow,
+			my_colorscheme.blue,
+			my_colorscheme.pink,
+			my_colorscheme.teal,
+			my_colorscheme.surface1,
+		},
+		indexed = { [16] = my_colorscheme.peach, [17] = my_colorscheme.rosewater },
+		tab_bar = {
+			active_tab = {
+				bg_color = my_colorscheme.sapphire,
+				fg_color = my_colorscheme.crust,
+			},
+			background = my_colorscheme.crust,
+			inactive_tab = {
+				bg_color = my_colorscheme.mantle,
+				fg_color = my_colorscheme.text,
+			},
+			inactive_tab_edge = my_colorscheme.surface0,
+			inactive_tab_hover = {
+				bg_color = my_colorscheme.base,
+				fg_color = my_colorscheme.text,
+			},
+			new_tab = {
+				fg_color = my_colorscheme.surface0,
+				bg_color = my_colorscheme.text,
+			},
+			new_tab_hover = {
+				fg_color = my_colorscheme.surface1,
+				bg_color = my_colorscheme.text,
+			},
+		},
+	},
+
+	-- Padding
+	window_padding = {
+		left = 25,
+		right = 25,
+		top = 25,
+		bottom = 0,
+	},
+
+	-- Tab Bar
+	enable_tab_bar = true,
+	hide_tab_bar_if_only_one_tab = true,
+	show_tab_index_in_tab_bar = false,
+	tab_bar_at_bottom = true,
+
+	-- General
+	automatically_reload_config = true,
+	inactive_pane_hsb = { saturation = 1.0, brightness = 1.0 },
+	window_background_opacity = 0.98,
+	window_close_confirmation = "NeverPrompt",
+	window_frame = {
+		active_titlebar_bg = my_colorscheme.base,
+		font = font_with_fallback(font_name, { bold = true }),
+	},
+}
