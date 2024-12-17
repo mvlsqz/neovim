d0f1f46c948ac47a94235cb38e6b8458dff28994
Marvin Velasquez <immavlgt@gmail.com>
2024-12-17 10:03:03 -0600

    fix(plugins): lazygit colors

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,19 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    lazygit = {
+      theme = {
+        [241] = { fg = "Special" },
+        activeBorderColor = { fg = "MatchParen", bold = true },
+        cherryPickedCommitBgColor = { fg = "Identifier" },
+        cherryPickedCommitFgColor = { fg = "Function" },
+        defaultFgColor = { fg = "Normal" },
+        inactiveBorderColor = { fg = "FloatBorder" },
+        optionsTextColor = { fg = "Function" },
+        searchingActiveBorderColor = { fg = "MatchParen", bold = true },
+        selectedLineBgColor = { bg = "Visual" },
+        unstagedChangesColor = { fg = "DiagnosticError" },
+      },
+    },
+  },
+}
