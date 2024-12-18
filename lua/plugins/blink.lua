19b5255df2bd80f0b8258d8e39fc362c2ba95bcb
Marvin Velasquez <immavlgt@gmail.com>
2024-12-18 08:00:27 -0600

    feat(plugins): better autocomplete mapping

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,7 +1,11 @@
 return {
   "saghen/blink.cmp",
   opts = {
-    keymap = { preset = "enter" },
+    keymap = {
+      preset = "enter",
+      ["<Tab>"] = { "select_next", "fallback" },
+      ["<S-Tab>"] = { "select_prev", "fallback" },
+    },
     sources = {
       default = { "lsp", "path", "snippets", "buffer", "markdown" },
       providers = {
